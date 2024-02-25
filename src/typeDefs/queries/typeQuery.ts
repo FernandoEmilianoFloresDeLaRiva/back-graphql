bf098a9b879b4e335add50185f18d924f07df91d
Roxana Flores <rextro@MacBook-Pro-de-Roxana.local>
2024-02-25 06:42:59 -0600

    login and typeQuery done

--- src/typeDefs/queries/typeQuery.ts
+++ src/typeDefs/queries/typeQuery.ts
@@ -1,9 +1,19 @@
+import { getAuthorByName } from "./author/getAuthorByName"
 import { getAuthors } from "./author/getAuthors"
+import { getBookByName } from "./book/getBookByName"
 import { getBooks } from "./book/getBooks"
+import { getBooksByAuthors } from "./book/getBooksByAuthor"
+import { login } from "./user/login"
 
 export const allQuerys : string =`
 type Query {
     ${getAuthors}
+    ${getAuthorByName}
+
     ${getBooks}
+    ${getBookByName}
+    ${getBooksByAuthors}
+    
+    ${login}
 }
 `
