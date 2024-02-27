2275d798aab8bc9e90ea9d15f9d067be6d68e62a
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-02-27 11:08:14 -0600

    .

--- src/services/authors/createAuthor.service.ts
+++ src/services/authors/createAuthor.service.ts
@@ -7,7 +7,6 @@ export const createAuthorService = async (
 ): Promise<CreateAuthorDto> => {
   try {
     await AuthorsRepository.createAuthor(name, lastName);
-
     return {
       name,
       lastName,
